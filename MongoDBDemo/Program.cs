d24a6256b11a046afa8b5fffeafb0bcfa36a8059
Steve Larsen, VRC Metal Systems <Steve.Larsen@vrcmetalsystems.com>
2020-08-04 15:45:14 -0600

    Add project files.

---
+++ MongoDBDemo/Program.cs
@@ -0,0 +1,82 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace MongoDBDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("AddressBook");
+
+            PersonModel person = new PersonModel
+            {
+                FirstName = "Joe",
+                LastName = "Smith",
+                PrimaryAddress = new AddressModel
+                {
+                    StreetAddress = "101 Oak Street",
+                    City = "Scranton",
+                    State = "PA",
+                    ZipCode = "18512"
+                }
+            };
+
+            db.InsertRecord("Users", person);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class PersonModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressModel PrimaryAddress { get; set; }
+    }
+
+    public class AddressModel
+    {
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.Find(filter).First();
+        }
+    }
+}
